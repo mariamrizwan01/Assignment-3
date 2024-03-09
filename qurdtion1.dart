6f3bd3a04139c9f892ad961192b1aa8983ae63f7
mariamrizwan01 <mariam.rizwan01@gmail.com>
2024-03-09 11:54:30 +0500

    Assigment 33

---
+++ qurdtion1.dart
@@ -0,0 +1,13 @@
+void main() {
+  int num = 0;
+  if (num > 0) {
+    print("$num is positive");
+  }
+ 
+ else if (num<0){
+  print("$num is not negative");
+ }
+ else {
+  print("$num is zero");
+ }
+}
